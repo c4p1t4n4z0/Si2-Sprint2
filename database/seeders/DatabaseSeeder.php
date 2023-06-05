b6afe976b78f41b4dc5c2d94862ee416923d846f
Cristian Cuellar <ccuellar260@gmail.com>
2023-06-05 17:47:55 -0400

    xd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,10 +32,10 @@ class DatabaseSeeder extends Seeder
         $user->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'; // password
         $user->remember_token = Str::random(10);
         $user->save();
-        
-        // $this->planes();
-        // $this->beneficios();
-        // $this->DetalleBeneficios();
+
+        $this->planes();
+        $this->beneficios();
+        $this->DetalleBeneficios();
 
         // $this->xd();
         // $this->areas();
